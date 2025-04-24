594887da063726aba2cb4606702e823cb9fc7ff7
erich <eric.hine2214@gmail.com>
2025-04-24 15:04:46 -0400

    Update config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
 
-def create_app(config_object="config.DevelopmentConfig"):
+from config import get_config
+
+def create_app():
+    config_class = get_config()
+    
     app = Flask(__name__)
-    app.config.from_object(config_object)
+    app.config.from_object(config_class)
     
     CORS(app, origins=app.config['CORS_ORIGINS'])
     
     from app.routes.api import api_bp
     app.register_blueprint(api_bp)
+
+    print(f"Starting app with config: {config_class.NAME}")
     
     return app
--- backend/config.py
+++ backend/config.py
@@ -5,18 +5,32 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-change')
     DEBUG = False
     TESTING = False
-    CORS_ORIGINS = ["http://localhost:5173"] 
+    CORS_ORIGINS = ["http://localhost:5173"]
+    NAME = "base"
 
 class DevelopmentConfig(Config):
     """Development configuration."""
     DEBUG = True
+    NAME = "development"
 
 class TestingConfig(Config):
     """Testing configuration."""
     TESTING = True
     DEBUG = True
+    NAME = "testing"
 
 class ProductionConfig(Config):
     """Production configuration."""
     DEBUG = False
     CORS_ORIGINS = ["https://productionfrontend.com"] # TODO : Change when have actual url.
+    NAME = "production"
+
+config_by_name = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
+
+def get_config():
+    env = os.environ.get('FLASK_ENV', 'development') 
+    return config_by_name.get(env, DevelopmentConfig)
