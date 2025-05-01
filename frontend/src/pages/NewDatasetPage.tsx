dfd4d58c4e55195fbb943b06eb1aa57696dc4a0d
erich <eric.hine2214@gmail.com>
2025-04-30 21:12:37 -0400

    Update new NewDatasetPage.

--- frontend/src/pages/NewDatasetPage.tsx
+++ frontend/src/pages/NewDatasetPage.tsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 
 function NewDatasetPage() {
   const navigate = useNavigate();
-
   const [topics, setTopics] = useState<string[]>([]);
   const [problemStatements, setProblemStatements] = useState<string[]>([]);
 
